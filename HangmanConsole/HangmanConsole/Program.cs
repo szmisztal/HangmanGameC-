2ce404dd3062e9a9da946b30e5a5d8b55215fb81
szmisztal <sz.misztal@gmail.com>
2024-07-28 16:17:23 +0200

    add some code lines to check that program works fine at the moment

--- HangmanConsole/HangmanConsole/Program.cs
+++ HangmanConsole/HangmanConsole/Program.cs
@@ -1,12 +1,22 @@
-﻿namespace HangmanConsole
+﻿using System.Diagnostics.Tracing;
+
+namespace HangmanConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<char> letters = new List<char>();
+
+            Console.WriteLine("Welcome to Hangman game !");
             HangmanGame game = new HangmanGame();
             game.AddGallowsDrawingsToDict();
-            game.DictPrint();
+            string word = game.WordDraw();
+            Console.WriteLine("Type a letter: ");
+            string letterString = Console.ReadLine();
+            char letter = letterString[0];
+            string hiddenWord = game.CheckLetterInWord(letters, word, letter);
+            Console.WriteLine(game.gallows[3]);
         }
     }
 }
