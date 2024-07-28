9aaad379b6d6afaa481b0e2928ea9b63529f1cb9
szmisztal <sz.misztal@gmail.com>
2024-07-28 16:16:50 +0200

    add gallows pictures dic, GuessedWord method

--- HangmanConsole/HangmanConsole/HangmanGame.cs
+++ HangmanConsole/HangmanConsole/HangmanGame.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,20 +22,42 @@ namespace HangmanConsole
             "Imperius Curse",
             "Locomotor Wibbly" };
         int mistakes = 0;
-        Dictionary<int, string> gallows = new Dictionary<int, string>();
+        public Dictionary<int, string> gallows = new Dictionary<int, string>();
 
         public void AddGallowsDrawingsToDict()
         {
             gallows.Add(1, "/");
             gallows.Add(2, "/ \\");
+            gallows.Add(3, " | \n/ \\");
+            gallows.Add(4, " | \n | \n/ \\");
+            gallows.Add(5, " | \n | \n | \n/ \\");
+            gallows.Add(6, " _\n | \n | \n | \n/ \\");
+            gallows.Add(7, " __\n | \n | \n | \n/ \\");
+            gallows.Add(8, " ___\n | \n | \n | \n/ \\");
+            gallows.Add(9, " ___\n | O\n | \n | \n/ \\");
+            gallows.Add(10, " ___\n | O\n |/\n | \n/ \\");
+            gallows.Add(11, " ___\n | O\n |/\\\n | \n/ \\");
+            gallows.Add(12, " ___\n | O\n |/|\\\n | \n/ \\");
+            gallows.Add(13, " ___\n | O\n |/|\\\n |/ \n/ \\");
+            gallows.Add(14, " ___\n | O\n |/|\\\n |/ \\ \n/ \\");
         }
 
-        public void DictPrint()
+        public string WordDraw()
         {
-            foreach (var entry in gallows)
+            return words[rng.Next(0, 10)];
+        }
+
+        public string GuessedWord(List<char> letters, string word, char letter)
+        {
+            letters.Add(letter);
+            string guessedWord = "";
+            foreach (char c in word)
             {
-                Console.WriteLine($"{entry.Key}: {entry.Value}");
+                if (letters.Contains(c)) { guessedWord += c; }
+                else if (c == ' ') {  guessedWord += " "; }
+                else { guessedWord += "_"; }
             }
+            return guessedWord;
         }
     }
 }
